de1d8bed9a206cb9dad6801f80b87e81d35d0587
gagandeeplt31-oss <gagandeeplt31@gmail.com>
2026-09-13 22:54:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from ultralytics import YOLO
 from PIL import Image
 import numpy as np
 import cv2
+from pillow_heif import register_heif_opener
+register_heif_opener()
+
 
 # ==============================================================================
 # 1. PAGE CONFIGURATION
